ef095bd4f23726907981475883d9239ad41bc386
Simon Huang <huangyuzhang@live.cn>
2019-05-13 14:25:14 +0100

    MOD: TOC js simplified

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,36 +24,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   
   });
-//
-  /*----------------------------------------------------*/
-  /*  Sticky Menu bar Scroll effect
-  /*----------------------------------------------------*/
-  var pxTocImg = 390; // height on which the button will show
-  var pxToc = 0;
-
-  $(window).scroll(function(){
-    // for post with featured image
-    if($(window).scrollTop() < pxTocImg){
-      $("#toc-img").addClass('is-absolute');
-      $("#toc-img").removeClass('is-fixed');
-    }
-    if($(window).scrollTop() >= pxTocImg){
-      $("#toc-img").removeClass('is-absolute');
-      $("#toc-img").addClass('is-fixed');
-    }
-
-    // for post without featured image
-    // if($(window).scrollTop() < pxToc){
-    //   $("#toc").addClass('is-absolute');
-    //   $("#toc").removeClass('is-fixed');
-    // }
-    // if($(window).scrollTop() >= pxToc){
-    //   $("#toc").removeClass('is-absolute');
-    //   $("#toc").addClass('is-fixed');
-    // }
-  });
-
-  //$('#flag a').on('click',function(){
-//	$("#float-canvas").removeClass('float-canvas-show');
-  //  return false;
-  //});
+/*----------------------------------------------------*/
+/*  Sticky TOC Scroll effect + 2019.05.12 -> 2019.05.13
+/*----------------------------------------------------*/
+$(window).scroll(function(){
+  var pxTocImg = 390;
+  // for post with featured image
+  if($(window).scrollTop() < pxTocImg){
+    $("#toc-img").addClass('is-absolute');
+    $("#toc-img").removeClass('is-fixed');
+  }
+  if($(window).scrollTop() >= pxTocImg){
+    $("#toc-img").removeClass('is-absolute');
+    $("#toc-img").addClass('is-fixed');
+  }
+});
